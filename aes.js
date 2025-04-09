cc18078225a801399ef9ce750ec44ca2982ce8bc
damianu <damian@datagentleman.io>
2025-04-09 02:36:23 +0200

    Add salt and error handling

--- aes.js
+++ aes.js
@@ -4,10 +4,12 @@ const crypto = window.crypto.subtle;
 function pbkdf2(password) {
   const encoder = new TextEncoder();
   const data = encoder.encode(password);
+  const usages = ['deriveBits', 'deriveKey'];
 
-  return crypto.importKey('raw', data, { name: 'PBKDF2' }, false, ['deriveBits', 'deriveKey']);
+  return crypto.importKey('raw', data, { name: 'PBKDF2' }, false, usages);
 }
 
+// AES-256
 function aes(pbkdf2, salt) {
   const algorithm = { name: "PBKDF2", salt: salt, iterations: 100_000, hash: "SHA-256" };
   const derived = { name: "AES-GCM", length: 256 };
@@ -19,20 +21,36 @@ function aes(pbkdf2, salt) {
 export class AES {
   constructor(password) {
     this.password = password;
-    this.salt = new ArrayBuffer();
-    this.iv = window.crypto.getRandomValues(new Uint8Array(12));
+    this.salt = window.crypto.getRandomValues(new Uint8Array(16));
 
     this.key = null;
     this.aes = null;
   }
 
   async encrypt(data) {
-    // Make sure that we calculate them only once
-    if (!this.key) { this.key = await pbkdf2(this.password) };
-    if (!this.aes) { this.aes = await aes(this.key, this.salt) };
+    try {
+      // Make sure that we calculate them only once
+      if (!this.key) { this.key = await pbkdf2(this.password) };
+      if (!this.aes) { this.aes = await aes(this.key, this.salt) };
 
-    const algorithm = { name: 'AES-GCM', iv: this.iv, additionalData: new Uint8Array(), tagLength: 128 };
+      const iv = window.crypto.getRandomValues(new Uint8Array(12));
+      const algorithm = { name: 'AES-GCM', iv: iv, tagLength: 128 };
 
-    return crypto.encrypt(algorithm, this.aes, data)
+      const cypher = await crypto.encrypt(algorithm, this.aes, data);
+      return { cypher: cypher, iv: iv, salt: this.salt };
+      
+    } catch (err) {
+      throw new Error(`AES encryption failed: ${err.message}`);
+    }
+  }
+
+  async decrypt(bytes, iv) {
+    try {
+      const algorithm = { name: "AES-GCM", iv };
+      return await crypto.decrypt(algorithm, this.aes, bytes);
+
+    } catch (err) {
+      throw new Error(`AES decryption failed: ${err.message}`);
+    }
   }
 }
