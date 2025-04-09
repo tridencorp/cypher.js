4cf2092b1a0dbc404aef375e4584bec21c63dc2e
damianu <damian@datagentleman.io>
2025-04-09 22:00:32 +0200

    Get keys from collection

--- db.js
+++ db.js
@@ -11,11 +11,34 @@ class DB {
     await new Promise((resolve, reject) => {
       const request = store.put(value, key);
 
-      request.onsuccess = () => resolve();
-      request.onerror = () => reject(request.error);
-    });
+      request.onsuccess = async () => {
+        tx.oncomplete = () => resolve();
+      }
 
-    await tx.done;
+      request.onerror = () => {
+        reject(request.error);
+        tx.onerror = () => reject(tx.error);  
+      }
+    });
+  }
+
+  // Get key from collection
+  async get(collection, key) {
+    const tx = this.db.transaction(collection, 'readonly');
+    const store = tx.objectStore(collection);
+
+    return new Promise((resolve, reject) => {
+      const request = store.get(key);
+
+      request.onsuccess = async () => {
+        tx.oncomplete = () => resolve(request.result);
+      };
+
+      request.onerror = () => {
+        reject('Failed to retrieve record');
+        tx.onerror = () => reject(tx.error);  
+      };
+    });
   }
 }
 
