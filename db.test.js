d1f711937b28b687df9692c0bd6010e64c66ff3a
damianu <damian@datagentleman.io>
2025-04-10 22:07:09 +0200

    More tests

--- db.test.js
+++ db.test.js
@@ -13,25 +13,29 @@ describe('DB', function () {
 
   it('should set and get keys from database', async function () {
     for (let i = 0; i < 1_000; i++) {
-      const item = { name: `item${i}`, value: i };
+      const item = { name: `key_${i}`, value: i };
       await db.set('addresses', `key_${i}`, item);
     }
 
     for (let i = 0; i < 1_000; i++) {
       const result = await db.get('addresses', `key_${i}`);
 
-      expect(result.name).to.equal(`item${i}`);
+      expect(result.name).to.equal(`key_${i}`);
       expect(result.value).to.equal(i);
     }
   });
 
   it('should get all keys from collection', async function () {
     for (let i = 0; i < 1_000; i++) {
-      const item = { name: `item${i}`, value: i };
+      const item = { name: `key_${i}`, value: i };
       await db.set('addresses', `key_${i}`, item);
     }
 
     const result = await db.all('addresses');
     expect(result.length).to.equal(1000);
+
+    for (let i = 0; i < 1_000; i++) {
+      expect(result[i][1].name).to.equal(result[i][0]);
+    }
   });
 });
