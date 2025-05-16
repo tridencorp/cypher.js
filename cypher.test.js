121cbb0731c3170735a673f44a1715fd1da72cc7
damianu <damian@datagentleman.io>
2025-05-16 23:58:20 +0200

    Fix tests

--- cypher.test.js
+++ cypher.test.js
@@ -1,16 +1,17 @@
 const { expect } = require('chai');
-const { Cypher, setupCryptp } = require('./aes');
+const { Cypher, setupCryptp } = require('./cypher');
 
 setupCryptp(globalThis.crypto);
 
 describe('Cypher', function () {
   it('should encrypt and decrypt data', async function () {
-    const data = new Uint8Array([1, 2, 3]).buffer
+    const data = "super secure data";
 
     const cypher = new Cypher("password");
-    const enc = await cypher.encrypt(data)
-    const res = await cypher.decrypt(enc)
+    let enc = await cypher.encrypt(new TextEncoder().encode(data));
+    let res = await cypher.decrypt(enc);
 
+    res = new TextDecoder().decode(res);
     expect(res).to.deep.equal(data);
   });
 });
